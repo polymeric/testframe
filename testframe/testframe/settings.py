b5d4156670aa666677fa5275f79ec3ef3603fc6f
polymeric <polymeric@users.noreply.github.com>
2018-04-03 21:51:08 -0400

    cleaning up settings.py comments

--- testframe/testframe/settings.py
+++ testframe/testframe/settings.py
@@ -21,8 +21,6 @@ from settings_secret import *
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
